b16d7f93dd0a5395c51d7c85b7fd4342ed036b5d
John Feng <johnfengphd@gmail.com>
2024-07-03 22:18:17 -0700

    add functions to calculate costs - add pandas to requirements

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 import streamlit as st
+import pandas as pd
+
 
 def api_key_check(openai_api_key):
     client = OpenAI(api_key=openai_api_key)
@@ -12,5 +14,24 @@ def api_key_check(openai_api_key):
             return True
     except Exception as e:
         return False
-    
-    
+
+
+prices = pd.read_csv(
+    r"assets\chat_completion_models.csv")
+
+
+def get_model_cost(model_choice):
+    """Returns the input and output cost of the model in cent/token."""
+    row = prices[prices["Model"] == model_choice]
+    input_cost = float(row["Input/1k Tokens"].values[0])/10  # cent/token
+    output_cost = float(row["Output/1k Tokens"].values[0])/10  # cent/token
+    return input_cost, output_cost
+
+
+def calc_total_cost(input_tokens, output_tokens, input_cost, output_cost):
+    """Output is in cents."""
+    return (input_tokens*input_cost + output_tokens*output_cost)
+
+
+if __name__ == "__main__":
+    print(get_model_cost("gpt-3.5-turbo"))
