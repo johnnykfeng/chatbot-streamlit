c6131d9822bf40d360da8e9df18a76d7f7a7f11e
John Feng <johnfengphd@gmail.com>
2024-07-05 20:10:44 -0700

    fix error where first message is assistant

--- pages/claude.py
+++ pages/claude.py
@@ -7,8 +7,8 @@ st.caption("🚀 A Streamlit chatbot powered by Anthropic")
 if "valid_key" not in st.session_state:
     st.session_state["valid_key"] = False
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+if "msg_history" not in st.session_state:
+    st.session_state["msg_history"] = []
 
 with st.sidebar:
     password = st.text_input("Password", value=None, type="password")
@@ -26,7 +26,7 @@ if not st.session_state["valid_key"]:
     st.info("Please add your Anthropic API key to continue.")
     st.stop()
 else:
-    for i, msg in enumerate(st.session_state.messages):
+    for i, msg in enumerate(st.session_state.msg_history):
         st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
@@ -36,20 +36,28 @@ if prompt := st.chat_input():
     
     client = anthropic.Anthropic(api_key=anthropic_api_key)
 
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.msg_history.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
+        
+        # Remove the first assistant message if it exists, avoid error in the API
+        if st.session_state.msg_history[0]["role"] == "assistant":
+            st.session_state.msg_history.pop(0)
+        
         response = client.messages.create(
             model=model_choice,
             max_tokens=1000,
             messages=[
                 {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages],
+                for m in st.session_state.msg_history],
             stream=False)
 
         msg = response.content[0].text
         st.markdown(msg)
 
-    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.session_state.msg_history.append({"role": "assistant", "content": msg})
+
+with st.expander("Show message history"):
+    st.write(st.session_state.msg_history)
